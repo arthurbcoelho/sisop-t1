3e71b8dd43a3ed1f9eca28077be733bc63128d99
Arthur <arthur.bortoluzzicoelho@gmail.com>
2022-04-09 09:08:33 -0300

    lastcmm

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <vector>
+#include <fstrea>
+
 
 using namespace std;
 
-struct Task {
+struct Task
+{
     int id;
     int start;
     int duration;
@@ -11,26 +14,43 @@ struct Task {
 
 int taskId = 0;
 int totalTime = 0;
+
 struct Task TASKS[] = {};
 
-void initialiseTask(struct Task &t, int start, int duration){
-    t.id = taskId;
-    t.start = start;
-    t.duration = duration;
 
-    TASKS[taskId] = t;
-    taskId++;
-    totalTime += duration;
-}
-
-void roundRobin(){
-    for(int i = 0; i < totalTime; i++){
-        cout << totalTime << endl;
+void initialiseTask(struct Task &t, int start, int duration)
+{
+    ifstream file("tarefas.txt");
+    if (file.is_open()){
+        string line;
+        while (getline(file, line)){
+            cout << line << endl; 
+        }
     }
+    
+    //t.id = taskId;
+    //t.start = start;
+    //t.executio
+    //t.duration = duration;
+//
+    //TASKS[taskId] = t;
+//
+    //taskId++;
+    //totalTime += duration;
 }
 
+void roundRobin()
+{
 
-int main() {
+    int QUANTUM = 2;
+    int taskCount = taskId;
+    int j = 0;
+
+    
+}
+
+int main()
+{
 
     int totalExecutionTime = 0;
     struct Task T1, T2, T3, T4;
@@ -41,14 +61,14 @@ int main() {
     initialiseTask(T4, 3, 5);
 
     roundRobin();
-    
+
     // cout << TASKS[0].id << endl;
     // cout << TASKS[1].id << endl;
     // cout << TASKS[2].id << endl;
     // cout << TASKS[3].id << endl;
     // cout << totalTime << endl;
-    //struct Task TASKS[] = {T1, T2, T3, T4}; // TODO Dinamic?
-    //roundRobin(TASKS, sizeof(TASKS) / sizeof(struct Task));
-    
+    // struct Task TASKS[] = {T1, T2, T3, T4}; // TODO Dinamic?
+    // roundRobin(TASKS, sizeof(TASKS) / sizeof(struct Task));
+
     return 0;
 }
